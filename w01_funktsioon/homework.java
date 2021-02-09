952658874859bb36a8a84c90c9e56d8eb091afb4
stellamarii <66253869+stellamarii@users.noreply.github.com>
2021-02-09 09:01:47 +0200

    Update homework.java

--- w01_funktsioon/homework.java
+++ w01_funktsioon/homework.java
@@ -3,6 +3,22 @@ import java.util.TimeZone;
 import java.util.Scanner;
 
 public class homework{
+
+    public static double age(double birthyear, double birthmonth, double birthday){
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        int day = calendar.get(Calendar.DATE);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int year = calendar.get(Calendar.YEAR);
+
+        double totaldays = (365.25 * birthyear) +  (30.44 * birthmonth) +  (birthday);
+
+        double nowdays = (365.25 * year) +  (30.44 * month) +  (day);
+
+        double age = round(nowdays - totaldays) / 365.25;
+
+        return age;
+    }
+
 	public static void main(String[] arg){
         
         Scanner input = new Scanner(System.in); 
@@ -12,25 +28,9 @@ public class homework{
         System.out.print("Sisesta sünnikuu: ");
         double birthmonth = input.nextDouble();
         System.out.print("Sisesta sünnipäev: ");
-        double birthday = input.nextDouble();
+        double birthday = input.nextDouble();      
+
+        System.out.println("Sinu vanus on: " +age(birthyear, birthmonth, birthday));
         
-        double totaldays = (365.25 * birthyear) +  (30.44 * birthmonth) +  (birthday) ;
-        //System.out.println("Päevi: " + birthday); 
-
-        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
-        int day = calendar.get(Calendar.DATE);
-        int month = calendar.get(Calendar.MONTH) + 1;
-        int year = calendar.get(Calendar.YEAR);
-
-        //System.out.println("Current Day is: " + day);
-        //System.out.println("Current Month is: " + month);
-        //System.out.println("Current Year is: " + year);
-
-        double nowdays = (365.25 * year) +  (30.44 * month) +  (day) ;
-        //System.out.println("Päevi: " + nowdays); 
-
-        double age = (nowdays - totaldays ) / 365.25 ; 
-
-        System.out.printf("Sinu vanus on:  %.1f\n", age);
     }
 }
